79a314235a207560c3e2345e3f879a994a3be85d
Tarun Chaudhary <tarun29061990@gmail.com>
2014-01-13 22:15:28 +0530

    Added google analytics

--- js/ga.js
+++ js/ga.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-39524883-1', 'tarun29061990.com');
+  ga('create', 'UA-39524883-2', 'curioustechie.in');
   ga('send', 'pageview');
